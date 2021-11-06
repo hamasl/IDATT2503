dcf21abc98cd6b976c00d1ed290d9aa56b7daa4f
Martin Hagen <martin@partins.no>
2021-11-06 19:02:12 +0100

    Think I completed task 3

--- Task8/3.py
+++ Task8/3.py
@@ -1,18 +1,177 @@
+"""
+NOTE: Rijndeal s-box and and inverse s-box was take from: https://medium.com/wearesinch/building-aes-128-from-the-ground-up-with-python-8122af44ebf9
+"""
+
+aes_sbox = [
+    [int('63', 16), int('7c', 16), int('77', 16), int('7b', 16), int('f2', 16), int('6b', 16), int('6f', 16),
+     int('c5', 16), int(
+        '30', 16), int('01', 16), int('67', 16), int('2b', 16), int('fe', 16), int('d7', 16), int('ab', 16),
+     int('76', 16)],
+    [int('ca', 16), int('82', 16), int('c9', 16), int('7d', 16), int('fa', 16), int('59', 16), int('47', 16),
+     int('f0', 16), int(
+        'ad', 16), int('d4', 16), int('a2', 16), int('af', 16), int('9c', 16), int('a4', 16), int('72', 16),
+     int('c0', 16)],
+    [int('b7', 16), int('fd', 16), int('93', 16), int('26', 16), int('36', 16), int('3f', 16), int('f7', 16),
+     int('cc', 16), int(
+        '34', 16), int('a5', 16), int('e5', 16), int('f1', 16), int('71', 16), int('d8', 16), int('31', 16),
+     int('15', 16)],
+    [int('04', 16), int('c7', 16), int('23', 16), int('c3', 16), int('18', 16), int('96', 16), int('05', 16),
+     int('9a', 16), int(
+        '07', 16), int('12', 16), int('80', 16), int('e2', 16), int('eb', 16), int('27', 16), int('b2', 16),
+     int('75', 16)],
+    [int('09', 16), int('83', 16), int('2c', 16), int('1a', 16), int('1b', 16), int('6e', 16), int('5a', 16),
+     int('a0', 16), int(
+        '52', 16), int('3b', 16), int('d6', 16), int('b3', 16), int('29', 16), int('e3', 16), int('2f', 16),
+     int('84', 16)],
+    [int('53', 16), int('d1', 16), int('00', 16), int('ed', 16), int('20', 16), int('fc', 16), int('b1', 16),
+     int('5b', 16), int(
+        '6a', 16), int('cb', 16), int('be', 16), int('39', 16), int('4a', 16), int('4c', 16), int('58', 16),
+     int('cf', 16)],
+    [int('d0', 16), int('ef', 16), int('aa', 16), int('fb', 16), int('43', 16), int('4d', 16), int('33', 16),
+     int('85', 16), int(
+        '45', 16), int('f9', 16), int('02', 16), int('7f', 16), int('50', 16), int('3c', 16), int('9f', 16),
+     int('a8', 16)],
+    [int('51', 16), int('a3', 16), int('40', 16), int('8f', 16), int('92', 16), int('9d', 16), int('38', 16),
+     int('f5', 16), int(
+        'bc', 16), int('b6', 16), int('da', 16), int('21', 16), int('10', 16), int('ff', 16), int('f3', 16),
+     int('d2', 16)],
+    [int('cd', 16), int('0c', 16), int('13', 16), int('ec', 16), int('5f', 16), int('97', 16), int('44', 16),
+     int('17', 16), int(
+        'c4', 16), int('a7', 16), int('7e', 16), int('3d', 16), int('64', 16), int('5d', 16), int('19', 16),
+     int('73', 16)],
+    [int('60', 16), int('81', 16), int('4f', 16), int('dc', 16), int('22', 16), int('2a', 16), int('90', 16),
+     int('88', 16), int(
+        '46', 16), int('ee', 16), int('b8', 16), int('14', 16), int('de', 16), int('5e', 16), int('0b', 16),
+     int('db', 16)],
+    [int('e0', 16), int('32', 16), int('3a', 16), int('0a', 16), int('49', 16), int('06', 16), int('24', 16),
+     int('5c', 16), int(
+        'c2', 16), int('d3', 16), int('ac', 16), int('62', 16), int('91', 16), int('95', 16), int('e4', 16),
+     int('79', 16)],
+    [int('e7', 16), int('c8', 16), int('37', 16), int('6d', 16), int('8d', 16), int('d5', 16), int('4e', 16),
+     int('a9', 16), int(
+        '6c', 16), int('56', 16), int('f4', 16), int('ea', 16), int('65', 16), int('7a', 16), int('ae', 16),
+     int('08', 16)],
+    [int('ba', 16), int('78', 16), int('25', 16), int('2e', 16), int('1c', 16), int('a6', 16), int('b4', 16),
+     int('c6', 16), int(
+        'e8', 16), int('dd', 16), int('74', 16), int('1f', 16), int('4b', 16), int('bd', 16), int('8b', 16),
+     int('8a', 16)],
+    [int('70', 16), int('3e', 16), int('b5', 16), int('66', 16), int('48', 16), int('03', 16), int('f6', 16),
+     int('0e', 16), int(
+        '61', 16), int('35', 16), int('57', 16), int('b9', 16), int('86', 16), int('c1', 16), int('1d', 16),
+     int('9e', 16)],
+    [int('e1', 16), int('f8', 16), int('98', 16), int('11', 16), int('69', 16), int('d9', 16), int('8e', 16),
+     int('94', 16), int(
+        '9b', 16), int('1e', 16), int('87', 16), int('e9', 16), int('ce', 16), int('55', 16), int('28', 16),
+     int('df', 16)],
+    [int('8c', 16), int('a1', 16), int('89', 16), int('0d', 16), int('bf', 16), int('e6', 16), int('42', 16),
+     int('68', 16), int(
+        '41', 16), int('99', 16), int('2d', 16), int('0f', 16), int('b0', 16), int('54', 16), int('bb', 16),
+     int('16', 16)]
+]
+
+reverse_aes_sbox = [
+    [int('52', 16), int('09', 16), int('6a', 16), int('d5', 16), int('30', 16), int('36', 16), int('a5', 16),
+     int('38', 16), int(
+        'bf', 16), int('40', 16), int('a3', 16), int('9e', 16), int('81', 16), int('f3', 16), int('d7', 16),
+     int('fb', 16)],
+    [int('7c', 16), int('e3', 16), int('39', 16), int('82', 16), int('9b', 16), int('2f', 16), int('ff', 16),
+     int('87', 16), int(
+        '34', 16), int('8e', 16), int('43', 16), int('44', 16), int('c4', 16), int('de', 16), int('e9', 16),
+     int('cb', 16)],
+    [int('54', 16), int('7b', 16), int('94', 16), int('32', 16), int('a6', 16), int('c2', 16), int('23', 16),
+     int('3d', 16), int(
+        'ee', 16), int('4c', 16), int('95', 16), int('0b', 16), int('42', 16), int('fa', 16), int('c3', 16),
+     int('4e', 16)],
+    [int('08', 16), int('2e', 16), int('a1', 16), int('66', 16), int('28', 16), int('d9', 16), int('24', 16),
+     int('b2', 16), int(
+        '76', 16), int('5b', 16), int('a2', 16), int('49', 16), int('6d', 16), int('8b', 16), int('d1', 16),
+     int('25', 16)],
+    [int('72', 16), int('f8', 16), int('f6', 16), int('64', 16), int('86', 16), int('68', 16), int('98', 16),
+     int('16', 16), int(
+        'd4', 16), int('a4', 16), int('5c', 16), int('cc', 16), int('5d', 16), int('65', 16), int('b6', 16),
+     int('92', 16)],
+    [int('6c', 16), int('70', 16), int('48', 16), int('50', 16), int('fd', 16), int('ed', 16), int('b9', 16),
+     int('da', 16), int(
+        '5e', 16), int('15', 16), int('46', 16), int('57', 16), int('a7', 16), int('8d', 16), int('9d', 16),
+     int('84', 16)],
+    [int('90', 16), int('d8', 16), int('ab', 16), int('00', 16), int('8c', 16), int('bc', 16), int('d3', 16),
+     int('0a', 16), int(
+        'f7', 16), int('e4', 16), int('58', 16), int('05', 16), int('b8', 16), int('b3', 16), int('45', 16),
+     int('06', 16)],
+    [int('d0', 16), int('2c', 16), int('1e', 16), int('8f', 16), int('ca', 16), int('3f', 16), int('0f', 16),
+     int('02', 16), int(
+        'c1', 16), int('af', 16), int('bd', 16), int('03', 16), int('01', 16), int('13', 16), int('8a', 16),
+     int('6b', 16)],
+    [int('3a', 16), int('91', 16), int('11', 16), int('41', 16), int('4f', 16), int('67', 16), int('dc', 16),
+     int('ea', 16), int(
+        '97', 16), int('f2', 16), int('cf', 16), int('ce', 16), int('f0', 16), int('b4', 16), int('e6', 16),
+     int('73', 16)],
+    [int('96', 16), int('ac', 16), int('74', 16), int('22', 16), int('e7', 16), int('ad', 16), int('35', 16),
+     int('85', 16), int(
+        'e2', 16), int('f9', 16), int('37', 16), int('e8', 16), int('1c', 16), int('75', 16), int('df', 16),
+     int('6e', 16)],
+    [int('47', 16), int('f1', 16), int('1a', 16), int('71', 16), int('1d', 16), int('29', 16), int('c5', 16),
+     int('89', 16), int(
+        '6f', 16), int('b7', 16), int('62', 16), int('0e', 16), int('aa', 16), int('18', 16), int('be', 16),
+     int('1b', 16)],
+    [int('fc', 16), int('56', 16), int('3e', 16), int('4b', 16), int('c6', 16), int('d2', 16), int('79', 16),
+     int('20', 16), int(
+        '9a', 16), int('db', 16), int('c0', 16), int('fe', 16), int('78', 16), int('cd', 16), int('5a', 16),
+     int('f4', 16)],
+    [int('1f', 16), int('dd', 16), int('a8', 16), int('33', 16), int('88', 16), int('07', 16), int('c7', 16),
+     int('31', 16), int(
+        'b1', 16), int('12', 16), int('10', 16), int('59', 16), int('27', 16), int('80', 16), int('ec', 16),
+     int('5f', 16)],
+    [int('60', 16), int('51', 16), int('7f', 16), int('a9', 16), int('19', 16), int('b5', 16), int('4a', 16),
+     int('0d', 16), int(
+        '2d', 16), int('e5', 16), int('7a', 16), int('9f', 16), int('93', 16), int('c9', 16), int('9c', 16),
+     int('ef', 16)],
+    [int('a0', 16), int('e0', 16), int('3b', 16), int('4d', 16), int('ae', 16), int('2a', 16), int('f5', 16),
+     int('b0', 16), int(
+        'c8', 16), int('eb', 16), int('bb', 16), int('3c', 16), int('83', 16), int('53', 16), int('99', 16),
+     int('61', 16)],
+    [int('17', 16), int('2b', 16), int('04', 16), int('7e', 16), int('ba', 16), int('77', 16), int('d6', 16),
+     int('26', 16), int(
+        'e1', 16), int('69', 16), int('14', 16), int('63', 16), int('55', 16), int('21', 16), int('0c', 16),
+     int('7d', 16)]
+]
+
+
 def encrypt_simple_aes_128(inp: [int], key: [int]) -> [int]:
     keyed_inp = add_round_key(inp, key)
-    print(keyed_inp)
     byte_matrix = [[-1] * 4 for _ in range(4)]
     for i in range(len(inp)):
-        byte_matrix[i % 4][i // 4] = keyed_inp[i]
-    print(byte_matrix)
+        byte_matrix[i % 4][i // 4] = s_box(keyed_inp[i])
+    shifted = shift_rows(byte_matrix)
+    out = []
+    for i in range(len(shifted)):
+        for j in range(len(shifted)):
+            out.append(shifted[j][i])
+    return out
 
 
-def decrypt_simple_aes_128(inp: [int]) -> [int]:
-    print("hei")
+def decrypt_simple_aes_128(inp: [int], key: [int]) -> [int]:
+    byte_matrix = [[-1] * 4 for _ in range(4)]
+    for i in range(len(inp)):
+        byte_matrix[i % 4][i // 4] = inp[i]
+    unshifted = unshift_rows(byte_matrix)
+    reversed = []
+    for i in range(len(unshifted)):
+        for j in range(len(unshifted)):
+            reversed.append(reverse_s_box(unshifted[j][i]))
+    # Add should do same as remove because of xor
+    return add_round_key(reversed, key)
 
 
-def s_box():
-    print("hei")
+def s_box(byte: int):
+    hex_str = format(byte, "x")
+    return aes_sbox[int(hex_str[0], 16)][int(hex_str[1], 16) if len(hex_str) > 1 else 0]
+
+
+def reverse_s_box(byte: int) -> int:
+    hex_str = format(byte, "x")
+    return reverse_aes_sbox[int(hex_str[0], 16)][int(hex_str[1], 16) if len(hex_str) > 1 else 0]
+
 
 def hex_to_byte_list(hex_str: str) -> [int]:
     out = []
@@ -48,17 +207,29 @@ def shift_rows(inp: [[int]]) -> [[int]]:
     return out
 
 
+def unshift_rows(inp: [[int]]) -> [[int]]:
+    out = []
+    for i in range(len(inp)):
+        new_row = [-1] * len(inp[i])
+        for j in range(len(inp[i])):
+            new_row[(j + i) % len(inp[i])] = inp[i][j]
+        out.append(new_row)
+    return out
+
+
 if __name__ == '__main__':
     key = "67 71 35 c4 ff da e5 ff 1c 54 e1 fd 7f 2e 88 b7"
 
     # Task a
-    m = "24 59 66 0c 99 da 9b 00 d6 55 fd 20 e9 ff 46 95"
+    m_a = "24 59 66 0c 99 da 9b 00 d6 55 fd 20 e9 ff 46 95"
+    c_a = encrypt_simple_aes_128(hex_to_byte_list(m_a), hex_to_byte_list(key))
+    print("Task a:")
+    print(f"Cipher as byte list: {c_a}")
+    print(f"Cipher as hex: {byte_list_to_hex(c_a)}")
 
     # Task b
-    c = "26 FA 83 E7 2D CD 5D B8 C4 DC EB 12 70 CF D16 1E"
-    print(hex_to_byte_list(key))
-    print(byte_list_to_hex(hex_to_byte_list(key)))
-
-    print(shift_rows([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]))
-
-    encrypt_simple_aes_128(hex_to_byte_list(m), hex_to_byte_list(key))
+    c_b = "26 FA 83 E7 2D CD 5D B8 C4 DC EB 12 70 CF D16 1E"
+    m_b = decrypt_simple_aes_128(hex_to_byte_list(c_b), hex_to_byte_list(key))
+    print("\n\nTask b:")
+    print(f"Cipher as byte list: {m_b}")
+    print(f"Cipher as hex: {byte_list_to_hex(m_b)}")
