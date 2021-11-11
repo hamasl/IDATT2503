dcdad73bf0e59c5f60ed807ffc1ecf3cdb5e0bf5
Martin Hagen <martin@partins.no>
2021-11-11 08:37:43 +0100

    Completed question 4c

--- Task9/4.py
+++ Task9/4.py
@@ -1,16 +1,32 @@
 import numpy as np
 
 
-def pollard_p_minus_one_unknown_a(n, b):
-    for i in range(2, n):
-        a = i ** (np.math.factorial(b)) % n
-        if (F := np.gcd(a - 1, n)) > 1:
-            return F
+def pollard_p_minus_one(n, b):
+    a = 2
+    for i in range(2, b + 1):
+        a = a ** i % n
+    if 1 < (F := np.gcd(a - 1, n)) < n:
+        return F
+    return None
+
+
+def pollard_p_minus_one_guess_b(n, max):
+    for i in range(max):
+        ans = pollard_p_minus_one(n, i)
+        if ans is not None:
+            return ans
+    return None
 
 
 if __name__ == '__main__':
     print("Task a:")
     n_a = 1829
-    ans = pollard_p_minus_one_unknown_a(n_a, 5)
-    print(f"ans: {ans}")
-    print(f"n/ans: {n_a / ans}")
+    ans_a = pollard_p_minus_one(n_a, 5)
+    print(f"ans: {ans_a}")
+    print(f"n/ans: {n_a / ans_a}")
+
+    print("\n\nTask c:")
+    n_c = 6319
+    ans_c = pollard_p_minus_one_guess_b(n_c, 12)
+    print(f"ans: {ans_c}")
+    print(f"n/ans: {n_c / ans_c}")
